96b36850b540412b4c9928cb77bcd560b6cb8f0d
Marshall <zz90847@gmail.com>
2021-09-15 20:02:52 +0300

    Added word B

--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
 class Hello {
-public static void main(String[] args) {
-        System.out.println("A");
-    }
+
     public static void main(String[] args) {
+System.out.println("B");
         System.out.println("Hello, World!");
     }
 }
